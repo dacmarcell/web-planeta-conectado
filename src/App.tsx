918051da8ec0bfcc33a18024d0371db126010dac
marcelldac <devmarcell@protonmail.com>
2024-07-21 03:54:45 -0300

    refactor: use post card hoc

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,13 @@
-import { useState } from 'react'
 import './App.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import PostList from './components/PostList'
+import PostCardHoc from './hoc/PostCardHoc'
 
 const queryClient = new QueryClient()
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <QueryClientProvider client={queryClient}>
-      <div>
-        <PostList/>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <PostCardHoc/>
     </QueryClientProvider>
   )
 }
