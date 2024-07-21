b3d98f2d371392c84686daa2dfb2ff8ef0cf8253
marcelldac <devmarcell@protonmail.com>
2024-07-21 18:33:17 -0300

    refactor: move query client provider to main

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import './App.css'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import PostCardHoc from './hoc/PostCardHoc'
 
-const queryClient = new QueryClient()
-
 function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-        <PostCardHoc/>
-    </QueryClientProvider>
+  return (    
+    <PostCardHoc/>
   )
 }
 
